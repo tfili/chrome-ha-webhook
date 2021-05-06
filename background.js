7cdc07fdeb40a5dbd89ad6fda967eda8f82f60ff
Tom Fili <tom@cesium.com>
2021-05-06 13:11:18 -0400

    Now notifies HA via a webhook on page load for Meet and Zoom

---
+++ background.js
@@ -0,0 +1,37 @@
+"use strict";
+
+let currentRequest;
+let config;
+getConfig();
+
+chrome.webNavigation.onBeforeNavigate.addListener(handler);
+
+function handler(details) {
+    if (config === undefined || currentRequest !== undefined) {
+        return;
+    }
+
+    const { webhook } = config;
+    const { url } = details;
+    if (/https:\/\/meet.google.com\/.+/.test(url) || /https:\/\/us02web.zoom.us\/.+/.test(url)) {
+        currentRequest = new XMLHttpRequest();
+        currentRequest.open(webhook.method, webhook.url);
+        currentRequest.onload = () => {
+            currentRequest = undefined;
+        };
+        currentRequest.onerror = (e) => {
+            alert(e);
+            currentRequest = undefined;
+        };
+        currentRequest.send();
+    }
+}
+
+function getConfig() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", chrome.extension.getURL('config.json'), true);
+    xhr.onload = () => {
+        config = JSON.parse(xhr.response);
+    };
+    xhr.send();
+}
